c399ea88f6224dc9f674c5829531f0ecc66e6a36
ghost <>
2020-03-03 15:38:15 +0800

    ccc

---
+++ TestSpring/TestSpring/src/main/java/com/gt/App.java
@@ -0,0 +1,15 @@
+package com.gt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!!!!!");
+		System.out.println("Hello!!!!!!!");
+        System.out.println("Hello Version1.1 !!!!!!");
+
+	}
+}
