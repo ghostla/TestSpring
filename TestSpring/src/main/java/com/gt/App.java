47a1e143bbb1752d231d2d192a44384369f38c6e
ghost <ghost@localhost>
2020-03-03 15:48:20 +0800

    merge程式

--- TestSpring/src/main/java/com/gt/App.java
+++ TestSpring/src/main/java/com/gt/App.java
@@ -4,29 +4,11 @@ package com.gt;
  * Hello world!
  *
  */
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 6eed348cf180836fa899e9fb3dd2734bde4dcf10
 public class App {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World!!!!!");
 		System.out.println("Hello!!!!!!!");
-<<<<<<< HEAD
-	}
-=======
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
         System.out.println("Hello Version1.1 !!!!!!");
-    }
->>>>>>> 9f973c104bf68ee322cf7649a33ef6e4cf9c458c
-=======
-        System.out.println("Hello Version1.1 !!!!!!");
-
 	}
->>>>>>> 6eed348cf180836fa899e9fb3dd2734bde4dcf10
 }
